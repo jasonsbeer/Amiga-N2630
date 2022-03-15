6e50d8da95cf40106549dbd15217c236949800c5
Jason Neus <78610265+jasonsbeer@users.noreply.github.com>
2022-03-14 21:28:18 -0500

    Delete Logic/N2630_LOW directory

--- Logic/N2630_LOW/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "/mnt/work/amiga/Peripherals/A2630/Recreate/Logic/N2630_LOW/MAIN_isim_beh.exe" -prj "/mnt/work/amiga/Peripherals/A2630/Recreate/Logic/N2630_LOW/MAIN_beh.prj" "work.MAIN" 
