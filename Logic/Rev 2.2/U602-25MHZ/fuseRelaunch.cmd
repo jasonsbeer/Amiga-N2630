25ee5f09ae439c2d43d53837462b95c6defd6e85
Jason Neus <78610265+jasonsbeer@users.noreply.github.com>
2022-12-12 21:18:27 -0600

    Add files via upload

---
+++ Logic/Rev 2.2/U602-25MHZ/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/mnt/work/amiga/Peripherals/A2630/Recreate/Logic/U602/U602_isim_beh.exe" -prj "/mnt/work/amiga/Peripherals/A2630/Recreate/Logic/U602/U602_beh.prj" "work.U602" 
