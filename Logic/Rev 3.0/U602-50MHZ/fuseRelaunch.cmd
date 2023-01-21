1266082df8fdd84b940e2bc4515696948e53e980
Jason Neus <78610265+jasonsbeer@users.noreply.github.com>
2023-01-20 23:54:40 -0600

    Delete Logic/Rev 3.0/U602-50MHZ directory

--- Logic/Rev 3.0/U602-50MHZ/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "/mnt/work/amiga/Peripherals/A2630/Recreate/Logic/U602-50MHZ/U602_isim_beh.exe" -prj "/mnt/work/amiga/Peripherals/A2630/Recreate/Logic/U602-50MHZ/U602_beh.prj" "work.U602" 
